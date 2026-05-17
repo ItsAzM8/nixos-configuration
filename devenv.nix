9ae6f492bf100988d04e06eb2556f826242531f6
Darcy Owens <owens.darcy67@gmail.com>
2026-05-17 07:46:30 +0000

    devenv cleanup

--- devenv.nix
+++ devenv.nix
@@ -9,34 +9,9 @@
     lsp.package = pkgs.nil;
   };
 
-  scripts = {
-    nixfmt = {
-      description = "Runs treefmt over all .nix files in the repo, excluding devenv.nix.";
-      exec = "treefmt .";
-
-      packages = with pkgs; [
-        nixfmt-tree
-      ];
-    };
-
-    nixbuild = {
-      description = "Builds the NixOS configuration using `nh`.";
-      exec = "nh os build --accept-flake-config";
-
-      packages = with pkgs; [
-        nh
-      ];
-    };
-
-    nixswitch = {
-      description = "Builds the NixOS configuration using `nh` and switches the system to it.";
-      exec = "nh os switch --accept-flake-config";
-
-      packages = with pkgs; [
-        nh
-      ];
-    };
-  };
+  packages = with pkgs; [
+    nixfmt-tree
+  ];
 
   git-hooks.hooks = {
     nixfmt.enable = true;
