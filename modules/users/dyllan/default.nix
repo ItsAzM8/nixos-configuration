8b0040fba0b840cb9578b190bbe72f0d1128636f
Darcy Owens <owens.darcy67@gmail.com>
2026-08-08 08:42:20 +0000

    Remove home manager.

--- modules/users/dyllan/default.nix
+++ modules/users/dyllan/default.nix
@@ -1,11 +1,8 @@
-{ self, ... }:
 {
   flake.nixosModules.dyllan-module =
     { ... }:
     {
-      imports = [
-        self.nixosModules.dyllan-home-manager
-      ];
       users.users.dyllan.isNormalUser = true;
+      programs.vscode.enable = true;
     };
 }
